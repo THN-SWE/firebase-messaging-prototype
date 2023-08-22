2e26ffb59f494d926fb133d17d1003459096c54c
thulasikan <thulasikanswe@gmail.com>
2023-08-22 20:42:50 +0530

    date break added

--- main.js
+++ main.js
@@ -87,8 +87,8 @@ function dateBreakEveryDay() {
     new Date().getFullYear(),
     new Date().getMonth(),
     new Date().getDate(),
-    0,
-    0,
+    20,
+    50,
     0,
     0
   ); // 10:00 AM
